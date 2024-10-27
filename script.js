ec003cd38407d68e925386cf202e977e945da9e0
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-10-27 20:29:03 +0530

    netflix page

---
+++ script.js
@@ -0,0 +1,9 @@
+function openbox(){
+    const openElement=document.getElementById('overlay');
+    openElement.style.display='flex';
+}
+
+function closebox(){
+    const openElement=document.getElementById('overlay');
+    openElement.style.display='none';
+}
